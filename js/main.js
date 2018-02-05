c7111457781e054f86d565714ef3173cd403afae
WilliamNua <William.Sparkes@student.nua.ac.uk>
2018-02-05 23:31:21 +0000

    Animations using atles and spritesheet

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ window.onload = function() {
 
     /* ====== global variable ====== */
     var player, keyboard, keyLeft, keyRight, keyDown, keyUp;
+    var isBusy = false;
 
     /* ====== create game object and canvas ====== */
     var game = new Phaser.Game(1280, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update, render:render });
@@ -13,7 +14,10 @@ window.onload = function() {
         game.stage.smoothed = false;
 
 
+        game.load.atlas('knightSheet', 'assets/characters/Knight/Knight_spritesheet.png', 'assets/characters/Knight/Knight.json');
+
         game.load.spritesheet('knight_idle', 'assets/characters/Knight/Knight_idle.png', 32, 32, 2);
+        game.load.spritesheet('knight_walk', 'assets/characters/Knight/Knight_walk.png', 32, 32, 8);
     }
 
 
@@ -22,16 +26,18 @@ window.onload = function() {
         // Player, camera and world setup
         game.world.setBounds(0, 0, 1280, 720);
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        player = game.add.sprite(64, 64, 'knight_idle');
+        player = game.add.sprite(64, 64, 'knightSheet');
         game.physics.arcade.enable(player);
+        player.anchor.setTo(.5,.5);
         cursors = game.input.keyboard.createCursorKeys();
         game.camera.follow(player);
-        game.camera.scale.x = 4;
-        game.camera.scale.y = 4;
+        game.camera.scale.x = 3.5;
+        game.camera.scale.y = 3.5;
 
         //animation
-        player.animations.add('idle');
-        player.animations.play('idle', 2, true);
+        player.animations.add('walk', Phaser.Animation.generateFrameNames('walk', 0, 7, '', 4), 15, true);
+        player.animations.add('idle', Phaser.Animation.generateFrameNames('idle', 0, 1, '', 4), 2, true);
+        player.animations.play('idle');
 
         // enable keyboard inputs
         keyboard = game.input.keyboard.createCursorKeys();
@@ -47,12 +53,18 @@ window.onload = function() {
         // reset players physics movement variable
         player.body.velocity.x = 0;
         player.body.velocity.y = 0;
+        isBusy = false;
 
         // check for inputss
-        if (keyLeft.isDown) 	{ player.body.velocity.x = -150; }	// move left
-        if (keyRight.isDown) 	{ player.body.velocity.x = 150; }		// move right
-        if (keyUp.isDown) 		{ player.body.velocity.y = -150; }	// move up
-        if (keyDown.isDown) 	{ player.body.velocity.y = 150; }		// move down
+        if (keyLeft.isDown) 	{ player.body.velocity.x = -100; player.scale.x = -1; player.animations.play('walk'); isBusy=true;}	// move left
+        if (keyRight.isDown) 	{ player.body.velocity.x = 100; player.scale.x = 1; player.animations.play('walk'); isBusy=true;}		// move right
+        if (keyUp.isDown) 		{ player.body.velocity.y = -100; player.animations.play('walk'); isBusy=true;}	// move up
+        if (keyDown.isDown) 	{ player.body.velocity.y = 100; player.animations.play('walk'); isBusy=true;}		// move down
+
+        // animations
+        if (isBusy === false) {
+            player.animations.play('idle');
+        }
     }
 
     /* ====== render ====== */
