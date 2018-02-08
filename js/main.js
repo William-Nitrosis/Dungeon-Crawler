5a069a33e353a3cd33ccfe0d1189bc2e2e9e7d6c
WilliamNua <William.Sparkes@student.nua.ac.uk>
2018-02-08 11:18:14 +0000

    Scaled all graphics by x4 to get zooming collisions to function properly, created a new level better suited to test ai path finding

--- js/main.js
+++ js/main.js
@@ -1,38 +1,59 @@
 window.onload = function() {
 
     /* ====== global variable ====== */
-    var player, keyboard, keyLeft, keyRight, keyDown, keyUp;
+    var player, keyboard, keyLeft, keyRight, keyDown, keyUp, wallsLayer, level1, background, floor, floorOverlay;
     var isBusy = false;
+    var movementSpeed = 200;
 
     /* ====== create game object and canvas ====== */
     var game = new Phaser.Game(1280, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update, render:render });
 
 
+
     /* ====== preload ====== */
     function preload() {
         // make everything CRISP
+        game.camera.scale.x = 1;
+        game.camera.scale.y = 1;
         game.stage.smoothed = false;
 
 
-        game.load.atlas('knightSheet', 'assets/characters/Knight/Knight_spritesheet.png', 'assets/characters/Knight/Knight.json');
+        game.load.atlas('knightSheet', 'assets/characters/Knight/Knight_spritesheetX4.png', 'assets/characters/Knight/Knight.json');
 
         game.load.spritesheet('knight_idle', 'assets/characters/Knight/Knight_idle.png', 32, 32, 2);
         game.load.spritesheet('knight_walk', 'assets/characters/Knight/Knight_walk.png', 32, 32, 8);
+
+        this.game.load.image('mapTiles', 'assets/tilesets/DarkDungeonv2_3x.png');
+        this.game.load.tilemap('level1', 'assets/tilesets/DungeonTest.json', null, Phaser.Tilemap.TILED_JSON);
+
+        game.time.advancedTiming = true;
     }
 
 
     /* ====== create ====== */
     function create() {
-        // Player, camera and world setup
-        game.world.setBounds(0, 0, 1280, 720);
+        // world setup
+        game.world.setBounds(0, 0, 5000, 5000);
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        player = game.add.sprite(64, 64, 'knightSheet');
+        cursors = game.input.keyboard.createCursorKeys();
+
+        // render tiled level map
+        level1 = game.add.tilemap('level1'); // step 1
+        level1.addTilesetImage('DungeonTiles', 'mapTiles'); // step 2
+        background = level1.createLayer('background');
+        floor = level1.createLayer('floor');
+        floorOverlay = level1.createLayer('floorOverlay');
+        wallsLayer = level1.createLayer('walls');
+        background.resizeWorld();
+        level1.setCollisionBetween(1, 3078, true, 'walls');
+
+        // spawn and setup player, camera
+        player = game.add.sprite(1300, 830, 'knightSheet');
         game.physics.arcade.enable(player);
         player.anchor.setTo(.5,.5);
-        cursors = game.input.keyboard.createCursorKeys();
         game.camera.follow(player);
-        game.camera.scale.x = 3.5;
-        game.camera.scale.y = 3.5;
+        player.body.setSize(45, 68, 27, 27);
+
 
         //animation
         player.animations.add('walk', Phaser.Animation.generateFrameNames('walk', 0, 7, '', 4), 15, true);
@@ -45,21 +66,23 @@ window.onload = function() {
         keyRight = game.input.keyboard.addKey(Phaser.Keyboard.D);
         keyDown = game.input.keyboard.addKey(Phaser.Keyboard.S);
         keyUp = game.input.keyboard.addKey(Phaser.Keyboard.W);
+
     }
 
 
     /* ====== update ====== */
     function update() {
+        game.physics.arcade.collide(wallsLayer, player);
         // reset players physics movement variable
         player.body.velocity.x = 0;
         player.body.velocity.y = 0;
         isBusy = false;
 
         // check for inputss
-        if (keyLeft.isDown) 	{ player.body.velocity.x = -100; player.scale.x = -1; player.animations.play('walk'); isBusy=true;}	// move left
-        if (keyRight.isDown) 	{ player.body.velocity.x = 100; player.scale.x = 1; player.animations.play('walk'); isBusy=true;}		// move right
-        if (keyUp.isDown) 		{ player.body.velocity.y = -100; player.animations.play('walk'); isBusy=true;}	// move up
-        if (keyDown.isDown) 	{ player.body.velocity.y = 100; player.animations.play('walk'); isBusy=true;}		// move down
+        if (keyLeft.isDown) 	{ player.body.velocity.x = -movementSpeed; player.scale.x = -1; player.animations.play('walk'); isBusy=true;}	// move left
+        if (keyRight.isDown) 	{ player.body.velocity.x = movementSpeed; player.scale.x = 1; player.animations.play('walk'); isBusy=true;}		// move right
+        if (keyUp.isDown) 		{ player.body.velocity.y = -movementSpeed; player.animations.play('walk'); isBusy=true;}	// move up
+        if (keyDown.isDown) 	{ player.body.velocity.y = movementSpeed; player.animations.play('walk'); isBusy=true;}		// move down
 
         // animations
         if (isBusy === false) {
@@ -72,6 +95,10 @@ window.onload = function() {
 
         game.debug.cameraInfo(game.camera, 32, 32);
         game.debug.spriteCoords(player, 32, 500);
+
+        game.debug.body(player);
+        //game.debug.body(wallsLayer);
+        game.debug.text('FPS: ' + game.time.fps || 'FPS: --', 30, 16, "#00ff00");
 
     }
 };
