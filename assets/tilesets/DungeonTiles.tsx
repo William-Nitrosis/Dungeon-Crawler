1f288cc49ea24baf39636699f3323a4072c6c492
WilliamNua <William.Sparkes@student.nua.ac.uk>
2018-02-08 11:19:16 +0000

    Uploading sprite sheets

---
+++ assets/tilesets/DungeonTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="DungeonTiles" tilewidth="48" tileheight="48" tilecount="78" columns="13">
+ <image source="DarkDungeonv2_3x.png" width="624" height="288"/>
+</tileset>
