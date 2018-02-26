04d59de60fef8b6eeb2cd43f8e93d23398f1f354
William-Nitrosis <33815671+William-Nitrosis@users.noreply.github.com>
2018-02-26 16:54:19 +0000

    First prototype!
    Added joystick and allowed canvas to render correctly on mobile devices.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
-window.onload = function() {
+//window.onload = function() {
 
     /* ====== global variable ====== */
-    var player, keyboard, keyLeft, keyRight, keyDown, keyUp, wallsLayer, level1, background, floor, floorOverlay;
+    var player, keyboard, keyLeft, keyRight, keyDown, keyUp, wallsLayer, level1, background, floor, floorOverlay, ai, pathfinder, walkables, path, pad, stick;
     var isBusy = false;
     var movementSpeed = 200;
 
     /* ====== create game object and canvas ====== */
-    var game = new Phaser.Game(1280, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update, render:render });
+    var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render:render });
 
 
 
     /* ====== preload ====== */
     function preload() {
+        // Window scaling
+        //this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        //this.game.scale.setShowAll();
+        //window.addEventListener('resize', function () {
+        //    this.game.scale.refresh();
+        //} );
+        //this.game.scale.refresh();
+
         // make everything CRISP
         game.camera.scale.x = 1;
         game.camera.scale.y = 1;
@@ -23,11 +31,23 @@ window.onload = function() {
         // Slime sheet and atlas
         game.load.atlas('slimeSheet', 'assets/ai/Slime/Slime_spritesheetX3.png', 'assets/ai/Slime/Slime.json');
 
-
+        // map tiles stuff
         game.load.image('mapTiles', 'assets/tilesets/DarkDungeonv2_3x.png');
         game.load.tilemap('level1', 'assets/tilesets/DungeonTest.json', null, Phaser.Tilemap.TILED_JSON);
 
+        // timing
         game.time.advancedTiming = true;
+
+        // ui
+        this.load.image('joystickBall', 'assets/ui/joystickBall.png');
+        this.load.image('joystickBackground', 'assets/ui/joystickBackground.png');
+        this.load.atlas('arcade', 'assets/ui/arcade-joystick.png', 'assets/ui/arcade-joystick.json');
+
+        // touch control
+        game.input.addPointer();
+        game.input.addPointer();
+        game.input.addPointer();
+        game.input.addPointer();
     }
 
 
@@ -54,13 +74,23 @@ window.onload = function() {
         player.anchor.setTo(.5,.5);
         game.camera.follow(player);
         //player.body.setSize(45, 68, 27, 27);
-        player.body.setSize(45, 10, 27, 86);
+        player.body.setSize(45, 55, 27, 41);
 
-
-        //animation
+        // player animation
         player.animations.add('walk', Phaser.Animation.generateFrameNames('walk', 0, 7, '', 4), 15, true);
         player.animations.add('idle', Phaser.Animation.generateFrameNames('idle', 0, 1, '', 4), 2, true);
         player.animations.play('idle');
+
+        // spawn ai
+        ai = game.add.sprite(1800, 830, 'slimeSheet');
+        game.physics.arcade.enable(ai);
+        ai.anchor.setTo(.5,.5);
+        ai.body.setSize(45, 47, 27, 49);
+
+        // slim animation
+        ai.animations.add('walk', Phaser.Animation.generateFrameNames('walk', 0, 7, '', 4), 15, true);
+        ai.animations.add('idle', Phaser.Animation.generateFrameNames('idle', 0, 1, '', 4), 2, true);
+        ai.animations.play('idle');
 
         // enable keyboard inputs
         keyboard = game.input.keyboard.createCursorKeys();
@@ -69,12 +99,29 @@ window.onload = function() {
         keyDown = game.input.keyboard.addKey(Phaser.Keyboard.S);
         keyUp = game.input.keyboard.addKey(Phaser.Keyboard.W);
 
+
+        // path finding
+        walkables = [41,42,43,44,54,55,28,29,30,31,23,24];
+        pathfinder = game.plugins.add(Phaser.Plugin.PathFinderPlugin);
+        pathfinder.setGrid(level1.layers[1].data, walkables);
+
+        // Joystick
+        pad = game.plugins.add(Phaser.VirtualJoystick);
+
+        stick = pad.addStick(0, 0, 200, 'arcade');
+        stick.alignBottomLeft();
+
+
+
+
     }
 
 
     /* ====== update ====== */
     function update() {
         game.physics.arcade.collide(wallsLayer, player);
+        game.physics.arcade.collide(wallsLayer, ai);
+        game.physics.arcade.overlap(ai, player, gameOver);
         // reset players physics movement variable
         player.body.velocity.x = 0;
         player.body.velocity.y = 0;
@@ -86,21 +133,82 @@ window.onload = function() {
         if (keyUp.isDown) 		{ player.body.velocity.y = -movementSpeed; player.animations.play('walk'); isBusy=true;}	// move up
         if (keyDown.isDown) 	{ player.body.velocity.y = movementSpeed; player.animations.play('walk'); isBusy=true;}		// move down
 
+        if (stick.isDown) {
+            isBusy = true;
+            //player.animations.play('walk');
+            this.physics.arcade.velocityFromRotation(stick.rotation, stick.force * movementSpeed, player.body.velocity);
+        }
+
+        if (stick.angle < 89 && stick.angle > -89) {
+            player.scale.x = 1;
+        } else {
+            player.scale.x = -1;
+        }
+
+        if(stick.isUp) {
+            isBusy = false;
+        }
+
         // animations
         if (isBusy === false) {
             player.animations.play('idle');
         }
+        else {
+            player.animations.play('walk');
+        }
+
+
+        // path finding
+        path = findPathTo(floor.getTileX(player.x+20), floor.getTileY(player.y+32));
+
+        if (path.length >= 4) {
+            game.physics.arcade.moveToXY(ai, path[3].x*48, path[3].y*48, 100);
+        } else {
+            game.physics.arcade.moveToXY(ai, path[1].x*48, path[1].y*48, 100);
+        }
+
+
     }
 
     /* ====== render ====== */
     function render() {
 
-        game.debug.cameraInfo(game.camera, 32, 32);
-        game.debug.spriteCoords(player, 32, 500);
+        //game.debug.cameraInfo(game.camera, 32, 32);
+        //game.debug.spriteCoords(player, 32, 500);
 
-        game.debug.body(player);
+        //game.debug.body(player);
+        //game.debug.body(ai);
         //game.debug.body(wallsLayer);
         game.debug.text('FPS: ' + game.time.fps || 'FPS: --', 30, 16, "#00ff00");
 
+        game.debug.pointer(game.input.mousePointer);
+        game.debug.pointer(game.input.pointer1);
+        game.debug.pointer(game.input.pointer2);
+        game.debug.pointer(game.input.pointer3);
+        game.debug.pointer(game.input.pointer4);
+        game.debug.pointer(game.input.pointer5);
+        game.debug.pointer(game.input.pointer6);
+
     }
-};
+
+    /* ====== path finding ====== */
+    function findPathTo(tilex, tiley) {
+        var goodPath = [];
+
+        pathfinder.setCallbackFunction(function(path) {
+            path = path || [];
+            goodPath = path;
+        });
+
+        pathfinder.preparePathCalculation([floor.getTileX(ai.x),floor.getTileY(ai.y)], [tilex,tiley]);
+        pathfinder.calculatePath();
+
+        return goodPath;
+
+    }
+
+    function gameOver() {
+        alert("Game over, you died");
+        window.location.reload();
+    }
+//};
